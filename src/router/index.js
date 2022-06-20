d0cc8f10e7849156a96cbd2f41a0b3d2b0a63ae2
OriamDev <iscac17783@alumni.iscac.pt>
2022-06-20 10:23:10 +0100

    0 - first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import TrendingMovies from "@/views/TrendingMovies";
+import TrendingHistoric from "@/views/TrendingHistoric";
+import MovieDetail from "@/views/MovieDetail";
 
 const routes = [
   {
@@ -8,12 +11,19 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/trending',
+    name: 'trending',
+    component: TrendingMovies
+  },
+  {
+    path: '/historic',
+    name: 'historic',
+    component: TrendingHistoric
+  },
+  {
+    path: '/movie/:id/:type',
+    name: 'movie',
+    component: MovieDetail
   }
 ]
 
---
+++ src/views/TrendingHistoric.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container" style="padding-bottom: 50px; padding-top: 150px;">
+    <div class="row justify-content-end">
+      <div class="col-lg-12">
+        <h1 class="mb-0" style="color: lightgray">Trending Historic</h1>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="row">
+
+        <div class="col-12" v-for="(item, index) in historic" :key="index">
+          <div class="box">
+           <div class="d-flex align-items-center">
+             <div class="text-white me-3">{{ index + 1 }}</div>
+             <h4 class="m-0">{{ getDateString(item.data) }}</h4>
+           </div>
+            <div class="movie">
+              <p class="text-white">
+                <span class="me-3">The Unbearable Weight of Massive Talent</span>
+                <span class="me-3"><b>Score: </b>7.2</span>
+                <span><b>Votes: </b>298</span>
+              </p>
+            </div>
+            <div class="actions">
+              <button class="btn btn-sm btn-outline-primary">Remove</button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TrendingHistoric",
+  data () {
+    return {
+      historic: null
+    }
+  },
+  mounted() {
+    this.getHistoric()
+  },
+  methods: {
+    getHistoric: async function () {
+      const response = await fetch('/historic.json')
+      const data = await response.json()
+      this.historic = data.results
+    },
+    getDateString (date) {
+      let myDate = new Date(date)
+      return myDate.toDateString()
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .box {
+    display: flex;
+    background: #171f38;
+    border: 1px solid transparent;
+    border-radius: 15px;
+    padding: 1.5rem;
+    margin: 1rem 0;
+    -webkit-transition: .3s ease-in-out;
+    transition: .3s ease-in-out;
+    justify-content: space-between;
+  }
+  .box h4 {
+    color: #FFFFFF;
+    font-size: 1.3rem;
+  }
+  p {
+    margin: 0;
+  }
+  button {
+    color: #eb4a4a;
+    border-color: #eb4a4a;
+  }
+  button:hover, .btn:focus {
+    color: lightgray;
+    background: #eb4a4a !important;
+    border-color: #eb4a4a !important;
+    box-shadow: none;
+  }
+</style>
